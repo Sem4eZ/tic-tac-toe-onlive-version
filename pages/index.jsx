fcd0118e68bcd3c0a314c891383907fbb7e868f1
Sem4eZ <semen.fedorov3005@yandex.ru>
2023-11-22 14:58:43 +0500

    add: gameInfo, gameTitle

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,15 @@
-import { Game } from "../components/game/game";
-import { Header } from "../components/header/";
+import { Header } from "../components/header/header";
+import { GameTitle } from "../components/game/game-title";
+import { GameInfo } from "../components/game";
 
 export default function HomePage() {
   return (
     <div className="bg-slate-50 min-h-screen">
       <Header />
+      <main className="pt-4 mx-auto max-w-[616px] ">
+        <GameTitle />
+        <GameInfo className="mt-4" />
+      </main>
     </div>
   );
 }
